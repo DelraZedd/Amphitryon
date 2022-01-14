b21103ff1c06d0832206848f90f7e5bcbde194b8
Arthur <arthurfrindelgmail.com>
2022-01-14 17:03:00 +0100

    commit

--- app/src/main/java/com/example/amphitryon/MainActivity.java
+++ app/src/main/java/com/example/amphitryon/MainActivity.java
@@ -3,8 +3,30 @@ package com.example.amphitryon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.IOException;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.OkHttpClient;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    String responseStr ;
+    OkHttpClient client = new OkHttpClient();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
